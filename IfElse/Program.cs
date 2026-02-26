f1d7c1b6344d796270a224d7d7157a410f6eff9b
Pedrosoares7i <phs087414.cxj@gmail.com>
2026-02-25 21:48:16 -0300

    Exemplos finalizados

---
+++ IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿// Pega a idade da pessoa
+int idade;
+Console.Write("Informe sua idade: ");
+int.TryParse(Console.ReadLine(), out idade); //Como a varável idade não tem valor, essa linha do código pega o que a pessoa informou na linha de Escreva sua idade pega e transforma em int, e a variável idade passa ter um valor
+
+//Pega o nome da pessoa
+string nome;
+Console.Write("Qual seu nome?: "); //Quando é string, não é necessário fazer a conversão
+
+//Pega o sexo da pessoa
+char sexo;
+Console.Write("Informe seu sexo: (M ou F)");
+char.TryParse(Console.ReadLine(), out sexo);
+
+if(sexo == 'F' && idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Você tem {idade} anos e é do gênero {sexo}, então pode entrar");
+}
+
+
+Console.ReadKey();
---
+++ for/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] diaSemana = new string[]
+{
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira"
+};
+
+for (int i = 0; i < diaSemana.Length; i++)
+{
+    Console.WriteLine(diaSemana[i]); // Console.Write aparece o resultado todo na mesma linha.
+                                     // Já o Console.WriteLine o resultado com quebra de linha
+}
+Console.ReadKey(); //Mantém o terminal aberto
